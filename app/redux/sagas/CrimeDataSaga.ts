a263b51b4af73fa092bdb6bf8a48ec52207b6175
rishijain2002 <rishijain0652@gmail.com>
2024-09-06 16:12:55 +0530

    give return type og fetchCrimeDataSaga

--- app/redux/sagas/CrimeDataSaga.ts
+++ app/redux/sagas/CrimeDataSaga.ts
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, PutEffect, takeLatest } from 'redux-saga/effects';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { fetchCrimeRequest, fetchCrimeSuccess, fetchCrimeFailure } from '../Slices/CrimeDstaSlice';
 interface CrimeData {
@@ -29,15 +29,22 @@ interface CrimeData {
       human_address: string;
     };
   }
-function fetchCrimeDataApi():Promise<AxiosResponse<CrimeData[]>> {
+function fetchCrimeDataApi():Promise<AxiosResponse<CrimeData[]>>{
   return axios.get(
     'https://data.cityofchicago.org/resource/ijzp-q8t2.json?$query=SELECT%0A%20%20%60id%60%2C%0A%20%20%60case_number%60%2C%0A%20%20%60date%60%2C%0A%20%20%60block%60%2C%0A%20%20%60iucr%60%2C%0A%20%20%60primary_type%60%2C%0A%20%20%60description%60%2C%0A%20%20%60location_description%60%2C%0A%20%20%60arrest%60%2C%0A%20%20%60domestic%60%2C%0A%20%20%60beat%60%2C%0A%20%20%60district%60%2C%0A%20%20%60ward%60%2C%0A%20%20%60community_area%60%2C%0A%20%20%60fbi_code%60%2C%0A%20%20%60x_coordinate%60%2C%0A%20%20%60y_coordinate%60%2C%0A%20%20%60year%60%2C%0A%20%20%60updated_on%60%2C%0A%20%20%60latitude%60%2C%0A%20%20%60longitude%60%2C%0A%20%20%60location%60%0AORDER%20BY%20%60date%60%20DESC%20NULL%20FIRST'
   );
 }
 
-function* fetchCrimeDataSaga(): Generator<any, void, AxiosResponse<CrimeData[]>> {
+function* fetchCrimeDataSaga(): Generator<
+  | ReturnType<typeof call>
+  | PutEffect<{ payload: CrimeData[]; type: "CrimeData/fetchCrimeSuccess" }>
+  | PutEffect<{ payload: string; type: "CrimeData/fetchCrimeFailure" }>,
+  void,
+  AxiosResponse<CrimeData[]>
+>
+{
   try {
-    const response = yield call(fetchCrimeDataApi);
+    const response: AxiosResponse<CrimeData[]> = yield call(fetchCrimeDataApi);
     yield put(fetchCrimeSuccess(response.data));
   } catch (error) {
     const axiosError = error as AxiosError;
